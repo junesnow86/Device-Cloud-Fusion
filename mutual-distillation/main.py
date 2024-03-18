12351e187a2353a19a36ec7bacd069fda9a2bfbf
junesnow86 <junliang@mail.ustc.edu.cn>
2024-03-18 06:56:54 +0000

    modify class distribution for each party

--- mutual-distillation/main.py
+++ mutual-distillation/main.py
@@ -162,36 +162,48 @@ cloud_size = len(train_data) // 10
 participant_size = len(train_data) - cloud_size
 cloud_train, parties_train = random_split(train_data, [cloud_size, participant_size])
 
-# Split participant data according to the class
+# Generate random proportion for each class
+class_proportions = np.random.dirichlet(np.ones(10), size=1)[0]
+
 labels = [y for _, y in parties_train]
-indices = np.array(range(len(labels)))
+party_A_indices, party_B_indices = [], []
+party_A_proportions, party_B_proportions = [], []
+for i in range(10):
+    class_indices = np.where(np.array(labels) == i)[0]
+    proportion = max(class_proportions[i], 1 - class_proportions[i])
+    split_index = int(proportion * len(class_indices))
 
-partyA_classes = [0, 1, 2, 3, 4]
-partyB_classes = [5, 6, 7, 8, 9]
-# partyA_classes = list(range(50))
-# partyB_classes = list(range(50, 100))
-
-partyA_indices = np.where(np.isin(labels, partyA_classes))[0]
-partyB_indices = np.where(np.isin(labels, partyB_classes))[0]
-
-partyA_train = Subset(parties_train, partyA_indices)
-partyB_train = Subset(parties_train, partyB_indices)
+    if i < 5:  # For the first 5 classes, assign more to A
+        party_A_indices.extend(class_indices[:split_index])
+        party_B_indices.extend(class_indices[split_index:])
+        party_A_proportions.append(proportion)
+        party_B_proportions.append(1 - proportion)
+    else:  # For the last 5 classes, assign more to B
+        party_B_indices.extend(class_indices[:split_index])
+        party_A_indices.extend(class_indices[split_index:])
+        party_B_proportions.append(proportion)
+        party_A_proportions.append(1 - proportion)
+print(f"Party A proportions: {party_A_proportions}")
+print(f"Party B proportions: {party_B_proportions}")
+exit()
+party_A_train = Subset(parties_train, party_A_indices)
+party_B_train = Subset(parties_train, party_B_indices)
 
 # Split validation data
 cloud_train_size = len(cloud_train) * 8 // 10
 cloud_val_size = len(cloud_train) - cloud_train_size
 cloud_train, cloud_val = random_split(cloud_train, [cloud_train_size, cloud_val_size])
 
-partyA_train_size = len(partyA_train) * 8 // 10
-partyA_val_size = len(partyA_train) - partyA_train_size
-partyA_train, partyA_val = random_split(
-    partyA_train, [partyA_train_size, partyA_val_size]
+party_A_train_size = len(party_A_train) * 8 // 10
+party_A_val_size = len(party_A_train) - party_A_train_size
+party_A_train, party_A_val = random_split(
+    party_A_train, [party_A_train_size, party_A_val_size]
 )
 
-partyB_train_size = len(partyB_train) * 8 // 10
-partyB_val_size = len(partyB_train) - partyB_train_size
-partyB_train, partyB_val = random_split(
-    partyB_train, [partyB_train_size, partyB_val_size]
+party_B_train_size = len(party_B_train) * 8 // 10
+party_B_val_size = len(party_B_train) - party_B_train_size
+party_B_train, party_B_val = random_split(
+    party_B_train, [party_B_train_size, party_B_val_size]
 )
 
 
@@ -221,21 +233,16 @@ train(cloud_model, cloud_train, cloud_val)
 cloud_acc = test(cloud_model, test_data)
 
 print(">>> Start training party A model...")
-train(party_A_model, partyA_train, partyA_val)
+train(party_A_model, party_A_train, party_A_val)
 party_A_acc = test(party_A_model, test_data)
 
 print(">>> Start training party B model...")
-train(party_B_model, partyB_train, partyB_val)
+train(party_B_model, party_B_train, party_B_val)
 party_B_acc = test(party_B_model, test_data)
 
 # Perform knowledge distillation from parties to the cloud
 print(">>> Start distillation from party A to cloud...")
-# distill(nn.Sequential(feature_extractor, party_A_backbone), nn.Sequential(feature_extractor, cloud_backbone), party_A_train, party_A_val, loss_fn_kd)
-# distil_train_data_A = ExtractedFeaturesDataset(partyA_train, party_A_fe)
-# distil_val_data_A = ExtractedFeaturesDataset(partyA_val, party_A_fe)
-# distil_cloud_data = ExtractedFeaturesDataset(cloud_train, cloud_fe)
-# distill(party_A_backbone, cloud_backbone, distil_train_data_A, distil_cloud_data)
-distill(party_A_model, cloud_model, partyA_train, cloud_train)
+distill(party_A_model, cloud_model, party_A_train, cloud_train)
 cloud_acc_kd1 = test(cloud_model, test_data)
 
 # print(">>> Start fine-tuning cloud model...")
@@ -247,7 +254,7 @@ print(">>> Start distillation from party B to cloud...")
 # distil_train_data_B = ExtractedFeaturesDataset(partyB_train, party_B_fe)
 # distil_val_data_B = ExtractedFeaturesDataset(partyB_val, party_B_fe)
 # distill(party_B_model, cloud_model, distil_train_data_B, distil_cloud_data)
-distill(party_B_model, cloud_model, partyB_train, cloud_train)
+distill(party_B_model, cloud_model, party_B_train, cloud_train)
 cloud_acc_kd2 = test(cloud_model, test_data)
 
 # print(">>> Start fine-tuning cloud model...")
@@ -257,12 +264,12 @@ cloud_acc_kd2 = test(cloud_model, test_data)
 # Perform KD from cloud backbone to parties
 print(">>> Start distillation from cloud to party A...")
 # distill(cloud_backbone, party_A_backbone, distil_cloud_data, distil_train_data_A)
-distill(cloud_model, party_A_model, cloud_train, partyA_train)
+distill(cloud_model, party_A_model, cloud_train, party_A_train)
 party_A_acc_kd = test(party_A_model, test_data)
 
 print(">>> Start distillation from cloud to party B...")
 # distill(cloud_backbone, party_B_backbone, distil_cloud_data, distil_train_data_B)
-distill(cloud_model, party_B_model, cloud_train, partyB_train)
+distill(cloud_model, party_B_model, cloud_train, party_B_train)
 party_B_acc_kd = test(party_B_model, test_data)
 
 # Print results
@@ -276,4 +283,3 @@ print(f"Party B accuracy after distillation from cloud: {party_B_acc_kd:.4f}")
 
 # TODO:
 # - 共享特征提取器和分类器层，但是需要处理non-iid所导致的参数加权平均效果不理想的问题
-# - 修改数据分布，参与方不是垄断某些类，而是每个类都有一定比例的数据
