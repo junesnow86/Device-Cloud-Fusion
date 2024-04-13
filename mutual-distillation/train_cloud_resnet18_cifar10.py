128b0a4de6c5af7056b4ff48df2f9f98eb0df4f1
junesnow86 <junliang@mail.ustc.edu.cn>
2024-04-13 12:04:33 +0000

    update

--- mutual-distillation/train_cloud_resnet18_cifar10.py
+++ mutual-distillation/train_cloud_resnet18_cifar10.py
@@ -3,11 +3,14 @@ from torchvision.models import resnet18
 
 from modules.data_utils import load_CIFAR10
 from modules.evaluation import test_accuracy
+from modules.models import ImageClassificationModel
 from modules.training import train
 
 train_data, val_data, test_data = load_CIFAR10(root="./data", download=False)
 
-model = resnet18(weights=None, num_classes=10)
+backbone_out_features = 256
+backbone = resnet18(weights=None, num_classes=backbone_out_features)
+model = ImageClassificationModel(backbone, backbone_out_features, num_classes=10)
 
 train(
     model,
@@ -15,24 +18,8 @@ train(
     val_data,
     batch_size=128,
     lr=0.1,
-    # checkpoint_save_path="./checkpoints/resnet18_cifar10.pth",
+    checkpoint_save_path="./checkpoints/custom_resnet18_cifar10.pth",
 )
 
 accuracy = test_accuracy(model, test_data)
 print(f"Test accuracy: {accuracy:.4f}")
-
-backbone = resnet18(weights=None, num_classes=256)
-classifier = torch.nn.Linear(256, 10)
-new_model = torch.nn.Sequential(backbone, torch.nn.ReLU(), classifier)
-
-train(
-    new_model,
-    train_data,
-    val_data,
-    batch_size=128,
-    lr=0.1,
-    # checkpoint_save_path="./checkpoints/resnet18_cifar10.pth",
-)
-
-accuracy = test_accuracy(new_model, test_data)
-print(f"Test accuracy with extra classifier: {accuracy:.4f}")
